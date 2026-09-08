7acdb7eba1ed0bbece8caab29cd3c6bfcdcae7d1
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-08 11:53:11 -0300

    feat: reactive component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CadastroAlunoTemplateComponent } from './components/cadastro-aluno-template/cadastro-aluno-template.component';
 import { CadastroAlunoReactiveComponent } from './components/cadastro-aluno-reactive/cadastro-aluno-reactive.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/cadastro-aluno-reactive/cadastro-aluno-reactive.component.ts
+++ src/app/components/cadastro-aluno-reactive/cadastro-aluno-reactive.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-aluno-reactive',
   templateUrl: './cadastro-aluno-reactive.component.html',
   styleUrls: ['./cadastro-aluno-reactive.component.css']
 })
+
 export class CadastroAlunoReactiveComponent {
+  form: FormGroup = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    nivel: new FormControl('', [Validators.required]),
+
+    cidade: new FormControl('', [Validators.required]),
+    estado: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+  });
+
+  onSubmit() {
+  
+      console.log('Aluno cadastrado:', this.form.valid);
+      // Aqui você pode adicionar a lógica para enviar os dados do aluno para um serviço ou API
+  }
 
 }
